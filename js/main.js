0c01a1e611a4b719dea14d14b45e63b6f0d3db95
HANNAH <hannahyi@uw.edu>
2023-10-22 10:56:27 -0700

    Main page edits

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Show Menu
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+//  Menu Show
+if(navToggle) {
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+//  Menu Hidden
+if(navClose) {
+    navClose.addEventListener('click', ()=>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+// Remove Menu Mobile
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
