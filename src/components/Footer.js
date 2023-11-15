e9cbd08e693ba053cf5eef6769486af09242080b
gsuyama3 <gsuyama3@uw.edu>
2023-11-15 13:19:05 -0800

    Export and import footer in most components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,9 @@
 import React from 'react';
+
+export function Footer(props) {
+    return (
+        <footer>
+            <p>&copy; Hannah Yi, Grace Suyama, Kayla Doan, Athena Le & INFO 340</p>
+        </footer>
+    );
+}
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,2 @@
 import React from 'react';
+import { Footer } from './Footer.js';
