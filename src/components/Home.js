b701e00805a56d26678d134f48548fb0c8a11fc0
HANNAH <hannahyi@uw.edu>
2023-11-15 13:32:48 -0800

    seperate into components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,2 +1,13 @@
 import React from 'react';
+
+import { NavigationBar } from './Navigation.js';
 import { Footer } from './Footer.js';
+
+export default function App(props) {
+    return (
+        <div>
+            <NavigationBar />
+            <Footer />
+        </div>
+    )
+}
