f6c70c71cdcb4011aeb6f47a6d3b82d4ed30ca07
HANNAH <hannahyi@uw.edu>
2023-11-20 16:35:38 -0800

    homepage link to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import { NavigationBar } from './Navigation.js';
+import { Home } from './Home.js';
 import { Footer } from './Footer.js';
 
 export default function App(props) {
     return (
         <div>
             <NavigationBar />
+            <Home />
             <Footer />
         </div>
     );
