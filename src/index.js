410af1034cc4f45e09d422f13079f1d2286be89b
kaylaxdoan <kaydoan@uw.edu>
2023-12-07 20:46:10 -0800

    add realtime database

--- src/index.js
+++ src/index.js
@@ -4,6 +4,24 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import './index.css';
 
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCI_r2YFvhuA6kytbqHEK3JjppZgFNYEK0",
+  authDomain: "info340-au23-brewlab.firebaseapp.com",
+  projectId: "info340-au23-brewlab",
+  storageBucket: "info340-au23-brewlab.appspot.com",
+  messagingSenderId: "344838453993",
+  appId: "1:344838453993:web:0266e91e1d10433d968e78"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <BrowserRouter>
