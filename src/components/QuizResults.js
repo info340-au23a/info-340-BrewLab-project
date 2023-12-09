8403e37ee39363daf938e9dae70ae25d92cf014c
gsuyama3 <gsuyama3@uw.edu>
2023-12-08 17:58:39 -0800

    Continue working on color change for selection in quiz, begin to try and organize cards for quiz results using state

--- src/components/QuizResults.js
+++ src/components/QuizResults.js
@@ -7,7 +7,7 @@ export function QuizResults(props) {
             <main className="quiz-page main-quiz-padding" >
                 <h1 className="quiz-header">Quiz Results</h1>
                 <div className="allCards">
-                    <Card />
+                    {/* <Card /> */}
                 </div>
             </main>
         </div>
