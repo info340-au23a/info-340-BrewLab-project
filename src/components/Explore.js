35ebfacc88fed0489cdd46c2e29d5d9cdcdeb0a2
gsuyama3 <gsuyama3@uw.edu>
2023-12-09 22:13:50 -0800

    Make an empty value for the explore filters

--- src/components/Explore.js
+++ src/components/Explore.js
@@ -26,9 +26,7 @@ export function Explore(props) {
     }
 
     const handleClickFilter = (event) => {
-        return (
-            <Card exploreFilter={userSelectedFilters} />
-        );
+        console.log(userSelectedFilters);
     }
 
     const optionCoffeeArray = coffeeTypes.map((coffeeType) => {
@@ -91,60 +89,3 @@ export function Explore(props) {
         </main>
     );
 }
-
-// export function Card(props) {
-//     const ingredients = props.ingredients;
-
-//     return (
-//         <div className="card">
-//             <div>
-//                 <div className="user-attribute">
-//                     <img src="/img/profile-picture.jpg" alt="avatar" className="avatar" />
-//                     <p className="avatarUsername">@athenalovescoffee</p>
-//                 </div>
-
-//                 <div>
-//                     <img className="coffeeimg" src="/img/dairyfreemocha.jpg" alt="coffee with ice" />
-//                     <h2>{props.drink}</h2>
-//                     <p>Short description of the drink</p>
-//                 </div>
-
-//                 <div className="sectionTracker">
-//                     <h3>Ingredients</h3>
-//                     <p>{ingredients.numShots} shots of a {ingredients.coffeeType}</p>
-//                     <p>{ingredients.milkVolume} of {ingredients.milkType}</p>
-//                     <p>{ingredients.sweetnessLevel}</p>
-//                     <p>{ingredients.drinkVolume}</p>
-//                     <p>{ingredients.syrupType} syrup</p>
-//                 </div>
-
-//                 <div className="sectionTracker">
-//                     <h3 className="h3tracker">Tags</h3>
-//                     <span className="tag">{ingredients.temperature}</span>
-//                     <span className="tag">{ingredients.tagMilkType}</span>
-//                     <span className="tag">{ingredients.syrupType}</span>
-//                 </div>
-//             </div>
-
-//             <div className="buttons">
-//                 <button className="primary-button">Add Drink</button>
-//                 <button className="favbutton">
-//                     <img className="favicon" src="/img/starv4x.png" alt="star icon" />
-//                 </button>
-//             </div>
-//         </div>
-//     );
-// }
-
-// export function AllCards(props) {
-//     const drinkArray = props.drinks.map((eachDrink) => {
-//         const aDrink = <Card key={eachDrink.drinkName} drink={eachDrink.drinkName} ingredients={eachDrink.ingredients} />
-//         return aDrink;
-//     })
-
-//     return (
-//         <div className="allCards">
-//             {drinkArray}
-//         </div>
-//     );
-// }
