852ad36a09a8d549bd544ee5fa8d919fc8c4fca7
athenaleh <uyenmile@uw.edu>
2023-12-09 19:42:48 -0800

    form working now! with saved data

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -31,13 +31,14 @@ export function Tracker(props) {
 
     const [formData, setFormData] = useState({
         drinkName: '',
-        coffeeType: '', 
+        drinkDescription: '',
+        coffeeType: 'Espresso', 
         temperature: '', 
-        drinkVolume: '', 
-        milkType: '', 
-        milkVolume: '',
-        foamVolume: '', 
-        sweetnessLevel: '', 
+        drinkVolume: 'extra small: 4 oz.', 
+        milkType: 'Whole', 
+        milkVolume: 'extra small: 4 oz.',
+        foamVolume: '30 mL', 
+        sweetnessLevel: '0% - no sugar', 
         syrupType: '', 
         syrupPumps: '',
     })
@@ -48,8 +49,9 @@ export function Tracker(props) {
             <h1 className="trackerHeader">Log Your Drink</h1>
             <form className="logging" onSubmit={handleSubmit}>
                 <DrinkName onChange={handleChange} formData={formData} />
+                <DrinkDescription onChange={handleChange} formData={formData} />
                 <CoffeeType onChange={handleChange} formData={formData} />
-                <TemperatureDrink onChange={handleChange} formData={formData} />
+                <TemperatureDrink temp={handleChange} formData={formData} />
                 <DrinkVolume onChange={handleChange} formData={formData} />
                 <MilkType onChange={handleChange} formData={formData} />
                 <MilkVolume onChange={handleChange} formData={formData} />
@@ -57,11 +59,18 @@ export function Tracker(props) {
                 <SweetLevel onChange={handleChange} formData={formData} />
                 <SyrupType onChange={handleChange} formData={formData} />
                 <SyrupPumps onChange={handleChange} formData={formData} />
-                <ImageUpload onChange={handleImageChange} />
+                <ImageUpload selectedImage={selectedImage} onChange={handleImageChange} />
                 <LogDrink />
             </form>
         </div>
     )
+
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    const handleImageChange = (event) => {
+        setSelectedImage(URL.createObjectURL(event.target.files[0]));
+    };
+
 
     const renderPostsContent = () => {
         // show cards of posted drinks here
@@ -71,17 +80,23 @@ export function Tracker(props) {
     
 
     const handleChange = (event) => {
-        const { name, value } = event.target;
+        var { name, value } = event.target;
+        console.log(event.target);
+        console.log(name);
+        console.log(value);
+
+        if (event.target.classList.contains("temperature")) {
+            name = "temperature";
+        } else {
+            name = event.target.id;
+        }
+
         setFormData((prevData) => ({
             ...prevData,
             [name]: value,
         }))
-    };
 
-    const [selectedImage, setSelectedImage] = useState(null);
-
-    const handleImageChange = (event) => {
-        setSelectedImage(URL.createObjectURL(event.target.files[0]));
+        console.log(event.target.classList);
     };
 
     const [submitted, setSubmitted] = useState(false);
@@ -134,8 +149,17 @@ export function Tracker(props) {
 function DrinkName(props) {
     return (
         <div className="tracker">
-            <label htmlFor="drink-name">Name of Drink</label>
-            <input id="drink-name" type="text" onChange={props.onChange} value={props.formData.drinkName} />
+            <label htmlFor="drinkName">Name of Drink</label>
+            <input id="drinkName" type="text" onChange={props.onChange} />
+        </div>
+    );
+}
+
+function DrinkDescription(props) {
+    return (
+        <div className="tracker">
+            <label htmlFor="drinkDescription">Describe Your Drink</label>
+            <input id="drinkDescription" type="text" onChange={props.onChange} />
         </div>
     );
 }
@@ -143,8 +167,8 @@ function DrinkName(props) {
 function CoffeeType(props) {
     return (
         <div className="tracker">
-            <label htmlFor="coffee-type">Choose the type of coffee you're drinking</label>
-            <select id="coffee-type" value={props.formData.coffeeType} onChange={props.onChange}>
+            <label htmlFor="coffeeType">Choose the type of coffee you're drinking</label>
+            <select id="coffeeType" onChange={props.onChange}>
                 <option value="espresso">Espresso</option>
                 <option value="macchiato">Macchiato</option>
                 <option value="cappucino">Cappucino</option>
@@ -158,16 +182,16 @@ function CoffeeType(props) {
     );
 }
 
-function TemperatureDrink() {
+function TemperatureDrink(props) {
     return (
         <section className="drink-temp">
             <label htmlFor="temperature">Temperature</label>
             <div>
-                <input type="radio" id="hot" name="drink" value="hot" />
+                <input type="radio" id="hot" className="temperature" name="temperature" value="hot" onChange={props.temp}/>
                 <label htmlFor="hot">Hot</label>
             </div>
             <div>
-                <input type="radio" id="iced" name="drink" value="iced" />
+                <input type="radio" id="iced" className="temperature" name="temperature" value="iced" onChange={props.temp}/>
                 <label htmlFor="iced">Iced</label>
             </div>
         </section>
@@ -177,8 +201,8 @@ function TemperatureDrink() {
 function DrinkVolume(props) {
     return (
         <div className="tracker">
-            <label htmlFor="drink-volume" className="explanation">Amount of coffee you made</label>
-            <select id="drink-volume" value={props.formData.drinkVolume} onChange={props.onChange}>
+            <label htmlFor="drinkVolume" className="explanation">Amount of coffee you made</label>
+            <select id="drinkVolume" onChange={props.onChange}>
                 <option value="xsmall">extra small: 4 oz.</option>
                 <option value="small">small: 6 oz.</option>
                 <option value="medium">medium: 8 oz.</option>
@@ -193,8 +217,8 @@ function DrinkVolume(props) {
 function MilkType(props) {
     return (
         <div className="tracker">
-            <label htmlFor="milk-type" className="explanation">Choose the type of milk you used</label>
-            <select id="milk-type" value={props.formData.milkType} onChange={props.onChange}>
+            <label htmlFor="milkType" className="explanation">Choose the type of milk you used</label>
+            <select id="milkType" onChange={props.onChange}>
                 <option value="whole">Whole</option>
                 <option value="soy">Soy</option>
                 <option value="hazelnut">Hazelnut</option>
@@ -212,8 +236,8 @@ function MilkType(props) {
 function MilkVolume(props) {
     return (
         <div className="tracker">
-            <label htmlFor="milk-volume" className="explanation">Amount of milk you added</label>
-            <select id="milk-volume" value={props.formData.milkVolume} onChange={props.onChange}>
+            <label htmlFor="milkVolume" className="explanation">Amount of milk you added</label>
+            <select id="milkVolume" onChange={props.onChange}>
                 <option value="xsmall">extra small: 4 oz.</option>
                 <option value="small">small: 6 oz.</option>
                 <option value="medium">medium: 8 oz.</option>
@@ -228,8 +252,8 @@ function MilkVolume(props) {
 function FoamVolume(props) {
     return (
         <div className="tracker">
-            <label htmlFor="foam-volume" className="explanation">Amount of foam you added</label>
-            <select id="foam-volume" value={props.formData.foamVolume} onChange={props.onChange}>
+            <label htmlFor="foamVolume" className="explanation">Amount of foam you added</label>
+            <select id="foamVolume" onChange={props.onChange}>
                 <option value="small">30mL</option>
                 <option value="medium">60mL</option>
                 <option value="large">120mL</option>
@@ -241,8 +265,8 @@ function FoamVolume(props) {
 function SweetLevel(props) {
     return (
         <div className="tracker">
-            <label htmlFor="sweet-level" className="explanation">Amount of sweetness level</label>
-            <select id="sweet-level" value={props.formData.sweetnessLevel} onChange={props.onChange}>
+            <label htmlFor="sweetnessLevel" className="explanation">Amount of sweetness level</label>
+            <select id="sweetnessLevel" onChange={props.onChange}>
                 <option value="zero">0% - no sugar</option>
                 <option value="25">25% - 1/4 sugar</option>
                 <option value="50">50% - 1/2 sugar</option>
@@ -257,8 +281,8 @@ function SweetLevel(props) {
 function SyrupType(props) {
     return (
         <div className="tracker">
-            <label htmlFor="syrup-type" className="explanation">Syrup You Used</label>
-            <input id="syrup-type" value={props.formData.syrupType} onChange={props.onChange} type="text" />
+            <label htmlFor="syrupType" className="explanation">Syrup You Used</label>
+            <input id="syrupType" onChange={props.onChange} type="text" />
         </div>
     );
 }
@@ -266,8 +290,8 @@ function SyrupType(props) {
 function SyrupPumps(props) {
     return (
         <div className="tracker">
-            <label htmlFor="syrup-pump" className="explanation">Pumps of Syrup</label>
-            <input id="syrup-pump" value={props.formData.syrupPumps} onChange={props.onChange} type="text" />
+            <label htmlFor="syrupPumps" className="explanation">Pumps of Syrup</label>
+            <input id="syrupPumps" onChange={props.onChange} type="text" />
         </div>
     );
 }
