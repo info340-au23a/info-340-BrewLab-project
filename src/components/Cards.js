eed46677b4e5c2f8c014823beb537ed1b9a169e4
gsuyama3 <gsuyama3@uw.edu>
2023-12-11 15:14:48 -0800

    Attempt to make the default screen of explore all the drinks

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -17,7 +17,7 @@ export function Card(props) {
 
         // get drink image
 
-        
+
 
         try {
             const newDrinkRef = push(drinksRef);
@@ -25,8 +25,8 @@ export function Card(props) {
             const storage = getStorage();
 
             await set(newDrinkRef, drink);
-        
-        }catch (error) {
+
+        } catch (error) {
             console.error('Error saving drink data to Firebase:', error);
         }
     };
@@ -42,19 +42,63 @@ export function Card(props) {
             const storage = getStorage();
 
             await set(newDrinkRef, drink);
-        
-        }catch (error) {
+
+        } catch (error) {
             console.error('Error saving drink data to Firebase:', error);
         }
     };
 
     // explore page filtering
     if (currentPage === "explore") {
+        console.log(exploreFilters);
         const ingredientsCoffeeType = ingredients.coffeeType.toLowerCase();
         const ingredientsMilkType = ingredients.milkType.toLowerCase();
         const ingredientsTemperature = ingredients.temperature.toLowerCase();
         const ingredientsSyrupType = ingredients.syrupType.toLowerCase();
 
+        // if (exploreFilters === null ||
+        //     exploreFilters.coffeeType === "any" &&
+        //     exploreFilters.milkType === "any" &&
+        //     exploreFilters.temperature === "any") {
+        //     return (
+        //         <div className="card">
+        //             <div>
+        //                 <div className="user-attribute">
+        //                     <img src="/img/profile-picture.jpg" alt="avatar" className="avatar" />
+        //                     <p className="avatarUsername">@athenalovescoffee</p>
+        //                 </div>
+
+        //                 <div>
+        //                     <img className="coffeeimg" src="/img/dairyfreemocha.jpg" alt="user's uploaded drink" />
+        //                     <h2>{props.drink}</h2>
+        //                     <p>Short description of the drink</p>
+        //                 </div>
+
+        //                 <div className="sectionTracker">
+        //                     <h3>Ingredients</h3>
+        //                     <p>{ingredients.numShots} shots of a {ingredients.coffeeType}</p>
+        //                     <p>{ingredients.milkVolume} of {ingredients.milkType} milk</p>
+        //                     <p>{ingredients.sweetnessLevel}</p>
+        //                     <p>{ingredients.drinkVolume}</p>
+        //                     <p>{ingredients.syrupType} syrup</p>
+        //                 </div>
+
+        //                 <div className="sectionTracker">
+        //                     <h3 className="h3tracker">Tags</h3>
+        //                     <span className="tag">{ingredients.temperature}</span>
+        //                     <span className="tag">{ingredients.tagMilkType} Milk</span>
+        //                     <span className="tag">{ingredients.syrupType}</span>
+        //                 </div>
+        //             </div>
+
+        //             <div className="buttons">
+        //                 <button className="primary-button" onClick={() => addDrink(ingredients)}>Add Drink</button>
+        //                 <button className="favbutton" onClick={() => savedDrink(ingredients)}>
+        //                     <img className="favicon" src="/img/starv4x.png" alt="star icon" />
+        //                 </button>
+        //             </div>
+        //         </div>
+        //     );
         if (ingredientsCoffeeType === exploreFilters.coffeeType ||
             ingredientsMilkType === exploreFilters.milkType ||
             ingredientsTemperature === exploreFilters.temperature ||
@@ -91,15 +135,16 @@ export function Card(props) {
                     </div>
 
                     <div className="buttons">
-                        <button className="primary-button" onClick={()=>addDrink(ingredients)}>Add Drink</button>
-                        <button className="favbutton" onClick={()=>savedDrink(ingredients)}>
+                        <button className="primary-button" onClick={() => addDrink(ingredients)}>Add Drink</button>
+                        <button className="favbutton" onClick={() => savedDrink(ingredients)}>
                             <img className="favicon" src="/img/starv4x.png" alt="star icon" />
                         </button>
                     </div>
                 </div>
             );
-        }
-    } // quiz results based off preference filtering
+        } 
+    }
+    // quiz filtering
     else if (currentPage === "quiz") {
         const quizCoffeeType = quizFilter.coffeeType;
         const lowercaseQuizCoffeeType = quizCoffeeType.toLowerCase();
@@ -140,8 +185,8 @@ export function Card(props) {
                     </div>
 
                     <div className="buttons">
-                        <button className="primary-button" onClick={()=>addDrink(ingredients)}>Add Drink</button>
-                        <button className="favbutton" onClick={()=>savedDrink(ingredients)}>
+                        <button className="primary-button" onClick={() => addDrink(ingredients)}>Add Drink</button>
+                        <button className="favbutton" onClick={() => savedDrink(ingredients)}>
                             <img className="favicon" src="/img/starv4x.png" alt="star icon" />
                         </button>
                     </div>
@@ -151,16 +196,16 @@ export function Card(props) {
     }
 }
 
-export function AllCards(props) {
-    const cardDrinkArray = props.drinks.map((eachDrink) => {
-        const aDrink = <Card key={eachDrink.drinkName} drink={eachDrink.drinkName} ingredients={eachDrink.ingredients} quizAnswers={props.quizAnswers} exploreFilters={props.exploreFilters} pageResult={props.pageResult} />
-        return aDrink;
-    })
+    export function AllCards(props) {
+        const cardDrinkArray = props.drinks.map((eachDrink) => {
+            const aDrink = <Card key={eachDrink.drinkName} drink={eachDrink.drinkName} ingredients={eachDrink.ingredients} quizAnswers={props.quizAnswers} exploreFilters={props.exploreFilters} pageResult={props.pageResult} />
+            return aDrink;
+        })
 
-    return (
-        <div className="allCards">
-            {cardDrinkArray}
-        </div>
-    );
-}
+        return (
+            <div className="allCards">
+                {cardDrinkArray}
+            </div>
+        );
+    }
 
