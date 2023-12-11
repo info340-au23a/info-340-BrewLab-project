fbce1cae7d5b8ef0b65a6c5e652426bde5d75a17
HANNAH <hannahyi@uw.edu>
2023-12-11 10:44:39 -0800

    new images + css edits

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export function Home() {
     return (
         <main className="main">
             {/* Section 1: Home */}
-            <div className="section1">
+            <div className="home-section">
                 <section className="home">
                     <div className="home-text">
                     <p>EXPLORE THE WORLD OF COFFEE</p>
@@ -16,32 +16,40 @@ export function Home() {
                 </section>
             </div>
 
-            {/* Section 2: Custom Orders */}
-            <div className="section2">
+            {/* Section 2: Custom Coffee Orders */}
+            <div className="orders-section">
                 <section className="orders">
                     <div className="heading">
                         <h1>Share Custom Orders</h1>
                     </div>
+                    <div className="home-order-img">
+                        <img src={process.env.PUBLIC_URL + '/img/explorepagepreview.png'} alt="preview of custom coffee explore page" />
+                    </div>
+                    <Link to="/explore" className="order-btn">Explore More</Link>
                 </section>
             </div>
 
             {/* Section 3: Tracker */}
-            <div className="section3">
+            <div className="tracker-section">
                 <section className="tracker">
                     <div className="heading">
                         <h1>Track Your Drinks</h1>
                     </div>
+                    <div className="home-tracker-img">
+                        <img src={process.env.PUBLIC_URL + '/img/trackerpreview.png'} alt="preview of the tracker page" />
+                    </div>
+                    <Link to="/tracker" className="order-btn">Log drink</Link>
                 </section>
             </div>
 
             {/* Section 4: Quiz */}
-            <div className="section4">
+            <div className="quiz-section">
                 <section className="quiz">
                     <div className="quiz-text">
                         <h1>Want to try something new?<div>Take our Coffee Quiz!</div></h1>
                         <Link to="/quiz" className="quiz-btn">Take Quiz</Link>
                     </div>
-                    <div className="home-img">
+                    <div className="home-quiz-img">
                         <img src="../img/yellowcup.png" alt="a yellow cup with coffee" /> 
                     </div>
                 </section>
@@ -49,3 +57,4 @@ export function Home() {
         </main>
     );
 }
+
